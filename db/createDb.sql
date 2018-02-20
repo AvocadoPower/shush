c719bd2fcb53558780501e396e9ef1b50905b1fb
Nicholas Teal <nteal15@gmail.com>
2018-02-20 15:03:38 -0600

    adds file to create local db in mysql

---
+++ db/createDb.sql
@@ -0,0 +1,4 @@
+-- if you have a password associated with mysql, run this file in same directory using:
+-- mysql -u <username> -p < createDb.sql 
+-- username may likely be 'root'
+CREATE DATABASE IF NOT EXISTS shush;
